cb1c2ff2236923026b55aa5da119c6d6f2b08187
Tasha <tashck@gmail.com>
2020-11-08 02:15:31 -0500

    modified golf club, created basic enemy health/death script

---
+++ Assets/Scripts/Enemy Scripts/Common/EnemyHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int startingHealth = 20;
+    public int currentHealth;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        Destroy(gameObject);
+    }
+}
