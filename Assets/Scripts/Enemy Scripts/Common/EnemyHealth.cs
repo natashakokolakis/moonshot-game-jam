9cd8479927e76b5ec0033741aeea702cabb59d5c
Chris North <chrisnorth90@gmail.com>
2020-11-08 17:33:08 -0500

    Enemy Health code cleaned up, started pushback methods

--- Assets/Scripts/Enemy Scripts/Common/EnemyHealth.cs
+++ Assets/Scripts/Enemy Scripts/Common/EnemyHealth.cs
@@ -1,15 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using ECM.Components;
+using ECM.Controllers;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    public int startingHealth = 20;
-    public int currentHealth;
+    #region Stats and Dependencies
+
+    [Header("Health Stats")]
+    [SerializeField]
+    private int startingHealth = 20;
+    [SerializeField]
+    private int currentHealth;
+    
+    // Used for pushing back enemy
+    private CharacterMovement enemyMovementController;
+
+
+    [Header("Weaknesses")]
+    [SerializeField]
+    private int meleeWeakness = 1;
+    [SerializeField]
+    private int rangedWeakness = 1;
+    [SerializeField]
+    private int specialWeakness = 1;
+
+    #endregion
+
+    private Vector3 CalculatePushback(int damage, int basePushback)
+    {
+        return Vector3.zero;
+    }
+
+    public void GetPushedBack(Vector3 pushback)
+    {
+        enemyMovementController.ApplyForce(pushback);
+    }
 
     private void Awake()
     {
         currentHealth = startingHealth;
+        enemyMovementController = this.GetComponent<CharacterMovement>();
     }
 
     public void TakeDamage(int amount)
@@ -20,10 +52,14 @@ public class EnemyHealth : MonoBehaviour
         {
             Death();
         }
+
+
     }
 
     void Death()
     {
         Destroy(gameObject);
     }
+
+
 }
