0eef2186835c9c0544c4a3e89669fd53097b8b55
Chris North <chrisnorth90@gmail.com>
2020-11-07 19:53:25 -0500

    Melee attack script started

---
+++ Assets/Scripts/Player Scripts/MeleeAttackScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttackScript : MonoBehaviour
+{
+    public BoxCollider meleeHitbox;
+    private void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
