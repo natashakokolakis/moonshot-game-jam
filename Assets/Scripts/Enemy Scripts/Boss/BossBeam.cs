f18d1c194575054cde342c1636676e312eedddca
Tasha <tashck@gmail.com>
2020-11-07 20:10:31 -0500

    no message

--- Assets/Scripts/Enemy Scripts/Boss/BossBeam.cs
+++ Assets/Scripts/Enemy Scripts/Boss/BossBeam.cs
@@ -6,13 +6,6 @@ public class BossBeam : MonoBehaviour
 {
     public int damage = 4;
 
-    Rigidbody rb;
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Player"))
