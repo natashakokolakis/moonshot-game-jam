ace002ee03c2486bc44cf72d048f427faea745dc
Chris North <chrisnorth90@gmail.com>
2020-11-20 22:34:04 -0500

    Tracking Icons added

--- Assets/Scripts/Utility/MapCameraController.cs
+++ Assets/Scripts/Utility/MapCameraController.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 
 public class MapCameraController : MonoBehaviour
 {
+    #region Variables and Dependencies
+
     private CinemachineVirtualCamera thisCamera;
     private Transform playerTrans;
     private Vector3 moveDirection;
     private PlayerMoonGolfController playerMoonGolfController;
     private float mapMoveSpeed = 20f;
-    private Vector3 cameraPosOffset = new Vector3(0, 19, -35);
+    private Vector3 cameraPosOffset = new Vector3(0, 30, 0);
     private GameObject mapModeCanvas;
+
+
+    // for zoom controls
+    private float defaultOrthoSize = 15;
+
+    public float maxOrthSize = 20;
+    public float minOrthSize = 10;
+    public float currentOrthSize;
+
+    public float zoomSpeed = 1f;
+
+
+    //To keep gameobject in the center of camera view
+    private Vector3 screenCentre = new Vector3(0.5f, 0.5f, 0);
+    private Camera mainCamera;
+    private bool cameraTransitionOver = false;
+    #endregion
+
 
 
     private void Awake()
     {
+        mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        currentOrthSize = defaultOrthoSize;
         mapModeCanvas = transform.Find("MapModeCanvas").gameObject;
         thisCamera = GetComponent<CinemachineVirtualCamera>();
         playerMoonGolfController = GameObject.Find("ECM_Player").GetComponent<PlayerMoonGolfController>();
@@ -23,12 +45,20 @@ public class MapCameraController : MonoBehaviour
 
     }
 
+    IEnumerator WaitforCameraTransition()
+    {
+        yield return new WaitForSeconds(1);
+        cameraTransitionOver = true;        
+    }
+
     public void TurnOnMapMode()
     {
         this.transform.position = playerTrans.position + cameraPosOffset;
+        thisCamera.m_Lens.OrthographicSize = defaultOrthoSize;
         thisCamera.Priority = 13;
         mapModeCanvas.SetActive(true);
         EventManagerNorth.TriggerEvent("ToggleGolfMode");
+        StartCoroutine(WaitforCameraTransition());
     }
 
     private void OnEnable()
@@ -43,6 +73,7 @@ public class MapCameraController : MonoBehaviour
         moveDirection = Vector3.zero;
         playerMoonGolfController.isInAOE = false;
         EventManagerNorth.TriggerEvent("ToggleGolfMode");
+        cameraTransitionOver = false;
         enabled = (false);
     }
 
@@ -58,6 +89,30 @@ public class MapCameraController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Tab))
             TurnOffMapMode();
 
+        if (Input.GetKey(KeyCode.Q))
+        {
+            DecreaseZoom();
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            IncreaseZoom();
+        }
+
+    }
+
+    private void IncreaseZoom()
+    {
+        thisCamera.m_Lens.OrthographicSize += zoomSpeed * Time.deltaTime;
+        if (thisCamera.m_Lens.OrthographicSize > maxOrthSize)
+            thisCamera.m_Lens.OrthographicSize = maxOrthSize;
+    }
+
+    private void DecreaseZoom()
+    {
+        thisCamera.m_Lens.OrthographicSize -= zoomSpeed * Time.deltaTime;
+        if (thisCamera.m_Lens.OrthographicSize < minOrthSize)
+            thisCamera.m_Lens.OrthographicSize = minOrthSize;
     }
 
     private void MoveCamera()
@@ -65,10 +120,22 @@ public class MapCameraController : MonoBehaviour
         transform.position += moveDirection * mapMoveSpeed * Time.deltaTime;
     }
 
+    private void MoveTransformToCentre()
+    {
+        transform.position = mainCamera.ViewportToWorldPoint(screenCentre);
+
+    }
+
     private void Update()
     {
         HandleInput();
         MoveCamera();
+    }
+
+    private void FixedUpdate()
+    {
+        if (cameraTransitionOver)
+        MoveTransformToCentre();
     }
 
 }
