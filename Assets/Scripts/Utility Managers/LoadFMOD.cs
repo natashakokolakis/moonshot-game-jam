72b2b120df271b96c542f67039f697b20c103f47
Tasha <tashck@gmail.com>
2020-11-28 03:46:40 -0500

    created FMOD loading scene

---
+++ Assets/Scripts/Utility Managers/LoadFMOD.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadFMOD : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (FMODUnity.RuntimeManager.HasBankLoaded("Master Bank"))
+        {
+            Debug.Log("Master Bank Loaded");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
