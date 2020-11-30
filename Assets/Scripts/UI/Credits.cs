4b993fb53359e0db8f898f45ab84d8f4eaf77898
Chris North <chrisnorth90@gmail.com>
2020-11-29 19:30:32 -0500

    bad branch

---
+++ Assets/Scripts/UI/Credits.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    public GameObject credits;
+    public GameObject endMessage;
+
+    void Awake()
+    {
+        StartCoroutine(RollCredits());
+    }
+
+    IEnumerator RollCredits()
+    {
+        yield return new WaitForSeconds(5);
+        endMessage.SetActive(false);
+        credits.SetActive(true);
+        StartCoroutine(ReturnToMainMenu());
+    }
+
+    IEnumerator ReturnToMainMenu()
+    {
+        yield return new WaitForSeconds(20);
+        SceneManager.LoadScene("Main Menu");
+    }
+}
